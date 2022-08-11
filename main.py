8729fd6b6ad639492f258af9a6719d03d42543f0
308 <308@academicos.cesde.edu.co>
2022-08-11 08:06:49 -0500

    Variables python

---
+++ main.py
@@ -0,0 +1,15 @@
+#1. DATOS PRIMITIVOS
+nombre ="Esteban"
+edad=20
+estatura=1.70
+hinchaDelMejor=True
+comidaFavorita=None
+
+#2. DATOS QUE ENTRAN POR EL TECLACO
+
+numero1=int (input("Digite un numero:"))
+numero2= int (input("digite numero2: "))
+
+suma = numero1 + numero2
+print(f'la suma de {numero1} y {numero2}es {suma}')
+
